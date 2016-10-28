eb8a8fe0a63214bc01dbd9dd71466715e161206e
Sm1else <dkuzmin@brite-apps.com>
2016-10-28 15:24:23 +0300

    Fixed previous commit

---
+++ Assets/Scripts/CrimeScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrimeScene : MonoBehaviour {
+
+	// Use this for initialization
+// 	void Start () {
+
+// 		WaitForSeconds(3.0f);
+		
+	
+// 	}
+	
+// 	// Update is called once per frame
+// 	void Update () {
+	
+// 	}
+
+public void PullBack()
+{
+	Application.LoadLevel(0);
+}
+
+}
---
+++ Assets/Scripts/Switcher.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Switcher : MonoBehaviour {
+
+	void Awake ()
+	{
+		// SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+
+	}
+	// Use this for initialization
+	// void Start () {
+	
+	// }
+	
+	// Update is called once per frame
+	// void Update () {
+	
+	// }
+
+	public void StartNewGame () 
+	{
+		Application.LoadLevel(1);
+		
+	// SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);	// SceneManager.SetActiveScene(1);
+	}
+
+	public void CrimeScene () 
+	{
+		Application.LoadLevel(2);
+		
+	// SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);	// SceneManager.SetActiveScene(1);
+	}
+}
