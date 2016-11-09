cd66d126df6bd9d4de3cbd101a015b248571220a
Sm1else <dkuzmin@brite-apps.com>
2016-11-09 18:51:05 +0300

    Added pause menu. Added basic text tutorial.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;	
 
@@ -9,39 +10,41 @@ public class GameManager : MonoBehaviour {
 
 	public bool firstRun;
 
-	public float gameSpeed;
-	public float influenceMax;
-	public float influenceRiseSpeed;
-	public float influenceFallSpeed;
-	public float coinFallSpeed;
-	public float coinRiseSpeed;
-	public float deposit;
-	public float initialPrice;
-	public float currentPrice;
-	public float supportPrice;
-	public float resistancePrice;
-	public float openPrice;
-	public float closePrice;
-	public float profit;
-	public int quantity;
-	public int cycleCounter;
-	public float coinPosY;
-	public float spread;
-	public float comission;
-	public float influence;
-	public float Devaluation;
-	float coinFallSpeedStorage;
+	public float gameSpeed;				//Общая скорость игры
+	public float influenceMax;			//Максимальное количество очков влияния
+	public float influenceRiseSpeed;	//Скорость восстановления очков влияния
+	public float influenceFallSpeed;	//Скорость растраты очков влияния
+	public float coinFallSpeed;			//Скорость падения монеты
+	public float coinRiseSpeed;			//Скорость подъёма монеты
+	public float deposit;				//Объём депозита (денег для торговли)
+	public float initialPrice;			//Начальная для игровой сессии цена инструмента (тут и далее: инструмент это пара товаров (Евро/Доллар, например))
+	public float currentPrice;			//Текущая цена инструмента
+	public float supportPrice;			//Значение цены уровня сопротивления (верхняя красная граница)
+	public float resistancePrice;		//Значение цены уровня поддержки (нижняя зелёная граница)
+	public float openPrice;				//Цена открытия позиции (начала сделки)
+	public float closePrice;			//Цена закрытия позиции (конца сделки)
+	public float profit;				//Доход от сделки
+	public int quantity;				//Количество товара торгуемого инстумента
+	public int cycleCounter;			//Глобальный счётчик длительности игровой сессии
+	public float coinPosY;				//Значение позиции монеты по Y (техническая переменная)
+	public float spread;				//Коэффициент разницы цен между ценой сопротивления и поддержки
+	public float comission;				//Размер комиссии, отчисляемой брокеру при открытии позиции
+	public float influence;				//Текущее количество очков влияния
+	public float Devaluation;			//Коэффициент обесценивания инструмента при падении ниже линии поддержки
+	float coinFallSpeedStorage;			
 	float coinRiseSpeedStorage;
 
-	public float topPositionProfit;
-	public float topSessionProfit;
-	public float overallCycles;
-	public float experience;
-	public float finalScore;
+	public float topPositionProfit;		//Самый большой доход по сделке
+	public float topSessionProfit;		//Самый большой доход за сессию 
+	public float overallCycles;			//Длительность игровой сессии
+	public float experience;			//Накопленный опыт
+	public float finalScore;			//Количество очков опыта
 
-	float stock;
+	float stock;						
 	float newSpread;
 	int newQuantity;
+	int gameState;
+	// int tutorStep;
 	bool gameOver;
 	bool positionOpen;
 	bool buying;
@@ -51,6 +54,9 @@ public class GameManager : MonoBehaviour {
 
 	public AudioClip[] audioClip;
 	AudioSource audioPlayer;
+
+	public List <float> pauseGameList = new List <float>(); 
+	public RectTransform[] tutorialPanels;
 	
 	public Scrollbar influenceBar;
 	public Scrollbar depositBar;
@@ -67,7 +73,9 @@ public class GameManager : MonoBehaviour {
 
 	public Scrollbar expBar;
 	public RectTransform gameOverPanel;
-	public RectTransform dialogPanel;
+	public RectTransform gamePausedPanel;
+	public RectTransform mainTutorialPanel;
+	// public RectTransform dialogPanel;
 	public Text tPPText;
 	public Text tSPText;
 	public Text sLenghtText;
@@ -83,12 +91,13 @@ public class GameManager : MonoBehaviour {
 		StartGame();
 		audioPlayer = GetComponent<AudioSource>();
 		gameOver = false;
+		PreRun ();
 
 	}
 
 	void PreRun()
 	{
-
+		if (firstRun == true) { RunTutorial (0); PauseGame(0, 0, influenceRiseSpeed, influenceFallSpeed, gameSpeed);}
 	}
 
 	public void StartGame()
@@ -99,6 +108,7 @@ public class GameManager : MonoBehaviour {
 		positionOpen = false;
 		quantity = 1;
 		gameOverPanel.gameObject.SetActive(false);
+		gamePausedPanel.gameObject.SetActive(false);
 	}
 
 	void Update() 
@@ -117,14 +127,14 @@ public class GameManager : MonoBehaviour {
 		if (positionOpen == true && buying == true)
 		{
 			profit = Mathf.Round(((currentPrice-openPrice)*quantity)*100.0f)/100.0f;
-			deposit-=comission;
+			if (gameState == 0) deposit-=comission;
 			Debug.Log("OP: " + openPrice + " | Current Price: " + currentPrice + " | Profit: " + profit);
 		}
 
 		else if (positionOpen == true && buying == false) 
 		{
 			profit = Mathf.Round(((openPrice-currentPrice)*quantity)*100.0f)/100.0f;
-			deposit-=comission;
+			if (gameState == 0) deposit-=comission;
 			Debug.Log("OP: " + openPrice + " | Current Price: " + currentPrice + " | Profit: " + profit);
 		}
 
@@ -165,32 +175,34 @@ public class GameManager : MonoBehaviour {
 
 	public void OutOfBounds (bool support, bool stay)
 	{
-
-		if (support == true && stay == true)
-		{	
-			coinFallSpeedStorage = coinFallSpeed;
-			coinFallSpeed = 0;
-			underSupport = true;
-		} 
-
-		if (support == false && stay == true) 
+		if (gameState == 0)
 		{
-			coinRiseSpeedStorage = coinRiseSpeed;
-			coinRiseSpeed = 0;
-			aboveResistance = true;
-		}
-		
+			if (support == true && stay == true)
+			{	
+				coinFallSpeedStorage = coinFallSpeed;
+				coinFallSpeed = 0;
+				underSupport = true;
+			} 
 
-		if (support == true && stay == false) 
-		{
-			coinFallSpeed = coinFallSpeedStorage;
-			underSupport = false;
-		}
+			if (support == false && stay == true) 
+			{
+				coinRiseSpeedStorage = coinRiseSpeed;
+				coinRiseSpeed = 0;
+				aboveResistance = true;
+			}
+			
 
-		if (support == false && stay == false)
-		{
-			coinRiseSpeed = coinRiseSpeedStorage;
-			aboveResistance = false;
+			if (support == true && stay == false) 
+			{
+				coinFallSpeed = coinFallSpeedStorage;
+				underSupport = false;
+			}
+
+			if (support == false && stay == false)
+			{
+				coinRiseSpeed = coinRiseSpeedStorage;
+				aboveResistance = false;
+			}
 		}
 
 		// else Debug.Log("RanAway");
@@ -268,6 +280,7 @@ public class GameManager : MonoBehaviour {
 			}
 
 			if (open == false)
+
 			{
 				openPrice = price;
 				{
@@ -304,12 +317,116 @@ public class GameManager : MonoBehaviour {
 
 	}
 
-	public void PauseGame (float coinFallSpd, float coinRsSpd, float infFallSpd, float gameSpd)
+	public void RunTutorial(int tutorStep)
 	{
+		if (tutorStep == 0)
+		{
+			mainTutorialPanel.gameObject.SetActive(true);
+			tutorialPanels[0].gameObject.SetActive(true);
+			tutorStep = 1;
+		} 
+
+		else if (tutorStep == 1)
+		{
+			tutorialPanels[0].gameObject.SetActive(false);
+			tutorialPanels[1].gameObject.SetActive(true);
+			tutorStep = 2;
+		}
+
+		else if (tutorStep == 2)
+		{
+			tutorialPanels[1].gameObject.SetActive(false);
+			tutorialPanels[2].gameObject.SetActive(true);
+			tutorStep = 3;
+		}
+
+		else if (tutorStep == 3)
+		{
+			tutorialPanels[2].gameObject.SetActive(false);
+			tutorialPanels[3].gameObject.SetActive(true);
+			tutorStep = 4;
+		}		
+
+		else if (tutorStep == 4)
+		{
+			tutorialPanels[3].gameObject.SetActive(false);
+			tutorialPanels[4].gameObject.SetActive(true);
+			tutorStep = 5;
+		}
+
+		else if (tutorStep == 5)
+		{
+			tutorialPanels[4].gameObject.SetActive(false);
+			tutorialPanels[5].gameObject.SetActive(true);
+			tutorStep = 6;
+		}
+
+		else if (tutorStep == 6)
+		{
+			tutorialPanels[5].gameObject.SetActive(false);
+			tutorialPanels[6].gameObject.SetActive(true);
+			tutorStep = 7;
+		}
+
+		else if (tutorStep == 7)
+		{
+			tutorialPanels[6].gameObject.SetActive(false);
+			firstRun = false;
+			mainTutorialPanel.gameObject.SetActive(false);
+			ResumeGame();
+		}
+
+		// else if (tutorStep == 8)
+		// {
+		// 	tutorialPanels[8].gameObject.SetActive(false);
+		// 	firstRun = false;
+		// 	mainTutorialPanel.gameObject.SetActive(false);
+		// 	ResumeGame();
+		// }
+
+	}
+
+	public void PauseButton()
+	{
+		if (gameState == 0)
+		{
+			PauseGame(0, 0, 0, 0, 0);
+			gameState = 1;
+			gamePausedPanel.gameObject.SetActive(true);
+
+		}
+
+		else if (gameState == 1)
+		{
+			ResumeGame();
+			gamePausedPanel.gameObject.SetActive(false);
+		}
+	}
+
+	public void PauseGame (float coinFallSpd, float coinRsSpd, float infFallSpd, float infRsSpd, float gameSpd)
+	{	
+		pauseGameList.Add (influenceRiseSpeed);
+		pauseGameList.Add (coinFallSpeed);
+		pauseGameList.Add (coinRiseSpeed);
+		pauseGameList.Add (influenceFallSpeed);
+		pauseGameList.Add (gameSpeed);
+
+		influenceRiseSpeed = infRsSpd;
 		coinFallSpeed = coinFallSpd;
 		coinRiseSpeed = coinRsSpd;
 		influenceFallSpeed = infFallSpd;
 		gameSpeed = gameSpd;	
+	}
+
+	public void ResumeGame()
+	{
+		influenceRiseSpeed = pauseGameList[0];
+		coinFallSpeed = pauseGameList[1];
+		coinRiseSpeed = pauseGameList[2];
+		influenceFallSpeed = pauseGameList[3];
+		gameSpeed = pauseGameList[4];
+		gameState = 0;
+		pauseGameList.Clear();
 	}
 
 	void ProgressStorage (float cycles, float tPP)
@@ -329,7 +446,7 @@ public class GameManager : MonoBehaviour {
 		gameOver = true;
 		gameOverPanel.gameObject.SetActive(true);
 		SoundManager(5);
-		PauseGame(4, 0, 0, 0);
+		PauseGame(4, 0, 0, 0, 0);
 		comission = 0;
 		deposit = 0;
 		profit = 0;
@@ -343,6 +460,7 @@ public class GameManager : MonoBehaviour {
 		expText.text = finalScore.ToString();
 		expBar.size = finalScore/100;
 		Debug.Log("OVERALL SCORE:  " + finalScore + "  || Cycles: " + overallCycles + " || TPP: " + topPositionProfit + " || TSP: " + topSessionProfit + "\n EXPERIENCE: " + experience);
+		gameState = 2;
 	}
 
 	public void MainMenu ()
