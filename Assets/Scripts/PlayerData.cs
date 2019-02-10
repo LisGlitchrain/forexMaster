c5fae4d7c88c2d4792d940fcfb6d8804948f91d8
Lis Glitchrain <hlambdafox@gmail.com>
2019-02-11 00:31:19 +0300

    Nope. Save and load are added in this commit.

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+[Serializable]
+
+public class PlayerData
+{
+    public float deposit;
+    public int quantity;
+}
---
+++ Assets/Scripts/PlayerDataSaverLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class PlayerDataSaverLoader {
+
+    public static PlayerData PlayerData { get; set; }
+    static string dataPath;
+
+    public static void Start()
+    {
+        dataPath = Path.Combine(Application.persistentDataPath, "CharacterData.txt");
+    }
+
+    public static void SavePlayerData(PlayerData data)
+    {
+        PlayerPrefs.SetString("characterName", "Default");
+        PlayerPrefs.SetFloat("deposit", data.deposit);
+        PlayerPrefs.SetInt("quantity", data.quantity);
+        PlayerPrefs.Save();
+    }
+
+    public static PlayerData LoadPlayerData()
+    {
+        PlayerData loadedCharacter = new PlayerData();
+        loadedCharacter.deposit = PlayerPrefs.GetFloat("deposit");
+        loadedCharacter.quantity = PlayerPrefs.GetInt("quantity");
+
+        return loadedCharacter;
+    }
+}
+
